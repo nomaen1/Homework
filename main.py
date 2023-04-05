58ec4e17f9d1c2b3bb27d142fa5ab237caacddcf
Janysh <tynybekovjanyshbek@gmail.com>
2023-04-05 18:56:14 +0600

    'DZ'

--- main.py
+++ main.py
@@ -11,5 +11,21 @@ async def start(message:types.Message):
 @dp.message_handler(commands = ['backend'])
 async def back(message:types.Message):
     await message.answer(f'Backend — это внутренняя часть сайта и сервера и т.д\nСтоимость 10000 сом в месяц\nОбучение: 5 месяц')
+    
+@dp.message_handler(commands = ['frontend'])
+async def back(message:types.Message):
+    await message.answer(f'Fronted — это внешняя часть сайта и сервера и т.д\nСтоимость 10000 сом в месяц\nОбучение: 5 месяц')
+    
+@dp.message_handler(commands = ['uiux'])
+async def back(message:types.Message):
+    await message.answer(f'UIUX — это дизайн сайта и сервера и т.д\nСтоимость 10000 сом в месяц\nОбучение: 5 месяц')
+    
+@dp.message_handler(commands = ['android'])
+async def back(message:types.Message):
+    await message.answer(f'Android — это создание приложения для устройств на операционной системе Android\nСтоимость 10000 сом в месяц\nОбучение: 5 месяц')
+    
+@dp.message_handler(commands = ['ios'])
+async def back(message:types.Message):
+    await message.answer(f'IOS — это это создание приложения для устройств на операционной системе IOS\nСтоимость 10000 сом в месяц\nОбучение: 5 месяц')
 
 executor.start_polling(dp)
